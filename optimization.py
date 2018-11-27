Fixed derivatives and added lipschitz step sizes

optimization.py
@@ -67,7 +67,7 @@ class optimizer:
 
             # update current 
             update = update.T.reshape(num_dimensions, 1)
-            alpha = self.step_function.step_size()
+            alpha = self.step_function.step_size(x_values[:, curr_iter - 1])
             x_values[:, curr_iter] = x_values[:, curr_iter - 1] - update.T * alpha / (
                     self.nu * curr_sigma * self.queries)
 
@@ -99,7 +99,7 @@ class optimizer:
                 curr_grad = oracle.derivative(x_values[:, curr_iter - 1])
                 cumulative_grad_magnitude += np.square(curr_grad)
 
-            alpha = self.step_function.step_size()
+            alpha = self.step_function.step_size(x_values[:, curr_iter - 1])
             x_values[:, curr_iter] = x_values[:, curr_iter - 1] - alpha / np.sqrt(cumulative_grad_magnitude) * curr_grad
 
             loss[curr_iter] = oracle.query_function(x_values[:, curr_iter])

oracle.py
@@ -7,7 +7,6 @@
 
  '''
 
-
 import numpy as np
 
 
@@ -21,36 +20,41 @@ class oracle:
     # Function Derivative
     def derivative(self, x):
         # Quadratic
+        mat = np.diag((self.condition_num, 1))
+
         if self.function == "QUADRATIC":
             # Parameter should be on the order of 0.1 to 10
-            return x * np.abs(x) / oracle.norm(x, self.function_param) ** (self.function_param - 1)
+            chain_rule = mat
+            inner = (mat @ x) * np.power(np.abs(mat @ x), self.function_param - 2)
+            return chain_rule @ inner
         elif self.function == "LOG":
             # Parameter should be on the order of 0.01 to 0.5
-            mat = np.diag((self.condition_num, 1))
-            return 2 * (mat.T @ mat @ x) / (np.linalg.norm(mat @ x) + self.function_param)
+            return 2 * (mat.T @ mat @ x) / (np.linalg.norm(mat @ x)**2 + self.function_param)
         return 0
 
+    # Function Derivative
+    def lipschitz(self, x):
+        # Quadratic
+        return np.linalg.norm(self.derivative(x))
+
     # Function Query 
-    # if function_num == 1, use a piecewise quadratic 
+    # if function_num == 1, use quadratic
     # if function_num == 2, use log function
     def query_function(self, x):
 
         mat = np.diag((self.condition_num, 1))
         # Quadratic
         if self.function == "QUADRATIC":
-            # Parameter should be on the order of 0.1 to 10
-            return oracle.norm(mat @ x, self.function_param)
+            return oracle.norm(mat @ x, self.function_param) ** self.function_param
 
         elif self.function == "LOG":
-
-            # Parameter should be on the order of 0.01 to 0.5
-
-            return np.log(np.linalg.norm(mat @ x) + self.function_param)
+            # Parameter should be on the order of 0.0001 to 0.1
+            return np.log(np.linalg.norm(mat @ x)**2 + self.function_param)
 
         return 0
 
     # Return the p-norm of a vector of an arbitrary p
     def norm(vec, p):
-        if np.linalg.norm(vec) == 0:
+        if np.all(vec==0):
             return 0
         return (sum(np.abs(vec) ** p) ** (1. / p)).squeeze()

plotting.py
@@ -39,6 +39,7 @@ def generate_plots(params, values, plot_type="Loss", optimizer_types=None):
     condition_num = oracle_params['CONDITION_NUM']
     initial_step_magnitude = step_params['INITIAL_STEP_MAGNITUDE']
 
+
     # Begin file name
     file_name = "{} Function ".format(function)
     plot_title = "{} Function ".format(function)
@@ -48,6 +49,8 @@ def generate_plots(params, values, plot_type="Loss", optimizer_types=None):
     if plot_type == "Surface" or plot_type == "Contour":
         function_oracle = oracle(oracle_params)
         optimizer_type = string_format(params["OPTIMIZER_TYPE"])
+        if step_size == "LIPSCHITZ" and optimizer_type != "NONNORMALIZE":
+            step_size = "INV_SQ_ROOT"
         file_name += "{} ".format(optimizer_type)
         plot_title += "{} ".format(optimizer_type)
         assert (num_dimensions == 2 and values.shape[0] == 2), \
@@ -66,8 +69,8 @@ def generate_plots(params, values, plot_type="Loss", optimizer_types=None):
         plt.xlim(right=max_x)
 
         # Calculate grid values
-        x = np.arange(-max_x - 0.01, max_x + 0.01, max_x / 50)
-        y = np.arange(-max_x - 0.01, max_x + 0.01, max_x / 50)
+        x = np.linspace(-max_x - 0.01, max_x + 0.01, 200)
+        y = np.linspace(-max_x - 0.01, max_x + 0.01, 200)
         X, Y = np.meshgrid(x, y)
         function_oracle = oracle(oracle_params)
         evaluated_grid = np.array(