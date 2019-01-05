Fixed empirical gradient calculations

main.py
@@ -96,6 +96,8 @@ def parse_args():
         'CONDITION_NUM': args.condition_num,
         'FUNCTION_PARAM': args.function_param
     }
+    if step_params['STEP_FUNCTION'] == "LIPSCHITZ":
+        oracle_params.update({'NU': args.nu, 'QUERIES': args.queries})
 
     return main_params, optimization_params, step_params, oracle_params
 
@@ -163,10 +165,7 @@ if __name__ == '__main__':
     # Step size parameter search
     if main_params["SEARCH"]:
         step_params["OPTIMAL"] = False
-
-
         optimal_step_sizes = []
-
 
         # Iterate over optimizer types
         for optimizer_type in optimizer_types:
@@ -190,7 +189,6 @@ if __name__ == '__main__':
                     average_losses.append(average_loss)
 
                 curr_optimal_step = np.linspace(start_step, end_step, num=sweep)[np.argmin(average_losses)]
-                # print(optimizer_type,curr_depth,curr_optimal_step)
                 start_step = max(curr_optimal_step - new_interval_length / 2, 0.0001)
                 end_step = curr_optimal_step + new_interval_length / 2
 

optimization.py
@@ -47,6 +47,7 @@ class optimizer:
         for curr_iter in range(1, self.iterations):
             curr_x = x_values[:, curr_iter - 1]
             alpha = self.step_function.step_size(curr_x)
+
             grad, sigma = self.get_derivative(curr_x, oracle)
 
             if self.optimizer_type == "NORMALIZE":
@@ -58,7 +59,6 @@ class optimizer:
             next_x = curr_x - alpha * grad
 
             # Add values
-
             x_values[:, curr_iter] = next_x
 
             loss[curr_iter] = oracle.query_function(next_x)
@@ -87,13 +87,12 @@ class optimizer:
         for curr_iter in range(1, self.iterations):
             curr_x = x_values[:, curr_iter - 1]
             alpha = self.step_function.step_size(curr_x)
-
             # Get the derivative
             curr_grad, sigma = self.get_derivative(curr_x.T, oracle)
             cumulative_grad_magnitude += np.square(curr_grad)
 
             # Update step with cumulative gradient magnitude
-            next_x = curr_x - alpha * curr_grad / np.sqrt(cumulative_grad_magnitude)
+            next_x = curr_x - alpha * curr_grad / np.sqrt(cumulative_grad_magnitude+1e-8)
 
             # Add values
             x_values[:, curr_iter] = next_x
@@ -109,8 +108,7 @@ class optimizer:
             grad = oracle.derivative(prev_x)
             return grad, None
         else:
-            empirical_grad, function_evals = oracle.empirical_derivative(prev_x, self.queries, self.nu)
-            #empirical_grad = empirical_grad.reshape(-1, 1)
+            empirical_grad, function_evals = oracle.empirical_derivative(prev_x, self.queries, self.nu, output_function_evals=True)
             function_var = np.var(function_evals)
             sigma = np.sqrt(function_var)
 

oracle.py
@@ -16,49 +16,9 @@ class oracle:
         self.condition_num = params["CONDITION_NUM"]
         self.function_param = params["FUNCTION_PARAM"]
         self.quadratic_threshold = 5
+        self.params = params
 
-    # Empirical Function Derivative
-    def empirical_derivative(self, x, num_queries, nu):
-        function_evals = np.zeros((num_queries, len(x)))
-        total_grad = np.zeros(len(x))
-        for i in range(num_queries):
-            # delta is the random perturbation to x_{j} to calculate approximate gradient
-            delta = np.random.normal(size=len(x))
-            # calculate the queried values x_{j} +/- nu*delta
-            pos_delta_x = x + nu * delta
-            neg_delta_x = x - nu * delta
-
-            function_evals[i, 0] = self.query_function(pos_delta_x.T)
-            function_evals[i, 1] = self.query_function(neg_delta_x.T)
-
-            # accumulate the update and multiply by delta
-            curr_grad = (function_evals[i, 0] - function_evals[i, 1]) * delta
-            total_grad += curr_grad
-
-        return total_grad / (nu * num_queries * 2) , function_evals
-
-        # Function Derivative
-
-    def derivative(self, x):
-        # Quadratic
-        mat = np.diag((self.condition_num, 1))
-
-        if self.function == "QUADRATIC":
-            # Parameter should be on the order of 0.1 to 10
-            chain_rule = mat
-            inner = (mat @ x) * np.power(np.abs(mat @ x), self.function_param - 2)
-            return chain_rule @ inner
-        elif self.function == "LOG":
-            # Parameter should be on the order of 0.01 to 0.5
-            return 2 * (mat.T @ mat @ x) / (np.linalg.norm(mat @ x) ** 2 + self.function_param)
-        return 0
-
-    # Function Derivative
-    def lipschitz(self, x):
-        # Quadratic
-        return np.linalg.norm(self.derivative(x))
-
-    # Function Query 
+    # Function Query
     # if function_num == 1, use quadratic
     # if function_num == 2, use log function
     def query_function(self, x):
@@ -66,7 +26,7 @@ class oracle:
         mat = np.diag((self.condition_num, 1))
         # Quadratic
         if self.function == "QUADRATIC":
-            return oracle.norm(mat @ x, self.function_param) ** self.function_param
+            return oracle.norm(mat @ x, self.function_param, raise_to_power=False)
 
         elif self.function == "LOG":
             # Parameter should be on the order of 0.0001 to 0.1
@@ -74,8 +34,83 @@ class oracle:
 
         return 0
 
+    # Function Derivative
+    def derivative(self, x):
+
+        mat = np.diag((self.condition_num, 1))
+
+        if self.function == "QUADRATIC":
+            # Parameter should be on the order of 0.1 to 10
+            chain_rule = self.function_param * mat.T
+            inner = (mat @ x) * np.power(np.abs(mat @ x), self.function_param - 2)
+            return chain_rule @ inner
+        elif self.function == "LOG":
+            # Parameter should be on the order of 0.01 to 0.5
+            return 2 * (mat.T @ mat @ x) / (np.linalg.norm(mat @ x) ** 2 + self.function_param)
+        return 0
+
+    # Empirical Function Derivative
+    def empirical_derivative(self, x, num_queries, nu, output_function_evals=False):
+
+        function_evals = np.zeros((num_queries, len(x)))
+        start_angle = np.random.rand()*2*np.pi
+        angles = np.linspace(start_angle, np.pi+start_angle, num_queries,endpoint=False)
+        deltas = np.array([np.cos(angles), np.sin(angles)]).T
+        pos_delta_x = x + nu * deltas
+        neg_delta_x = x - nu * deltas
+        function_evals[:, 0] = np.apply_along_axis(func1d=self.query_function, arr=pos_delta_x, axis=1)
+        function_evals[:, 1] = np.apply_along_axis(func1d=self.query_function, arr=neg_delta_x, axis=1)
+        function_at_point = self.query_function(x)
+        function_diff = (function_evals[:, 0] - function_evals[:, 1]).reshape(-1, 1)
+        # one half from sampling from both sides,
+        # one half from expected value of each coordinate in delta
+        grad = function_diff * deltas / nu
+        total_grad = np.sum(grad, axis=0) / (num_queries)
+        if output_function_evals:
+            return total_grad, function_evals
+        else:
+            return total_grad
+
+        # Empirically calculate lipschitz constant
+        # Consider points of radius 1e-8 around the given point
+        # Find the largest ratio of (f'(x)-f'(y))/(x-y)
+
+    def lipschitz(self, x, gradient_type, num_samples=20, eps=1e-3, min_value=2):
+        if gradient_type == "EMPIRICAL":
+            nu = self.params["NU"]
+            num_queries = self.params["QUERIES"]
+
+        coords = np.random.randn(num_samples, len(x))
+        r = np.linalg.norm(coords, axis=1).reshape(-1, 1)
+        points_on_sphere = (coords / r) * eps
+        shifted_points = points_on_sphere + x
+        if gradient_type == "TRUE":
+            derivatives = np.apply_along_axis(func1d=self.derivative, axis=1, arr=shifted_points)
+            curr_derivative = self.derivative(x)
+        else:
+            true_derivatives = np.apply_along_axis(func1d=self.derivative, axis=1, arr=shifted_points)
+            true_curr_derivative = self.derivative(x)
+            true_derivative_diff = np.linalg.norm(true_curr_derivative - true_derivatives, axis=1)
+
+            true_ratios = true_derivative_diff / eps
+            true_lipschitz_constant = np.max(true_ratios)
+            derivatives = np.apply_along_axis(func1d=self.empirical_derivative, axis=1, arr=shifted_points, nu=1e-3,
+                                              num_queries=10)
+            curr_derivative = self.empirical_derivative(x, nu=1e-3,
+                                                        num_queries=100)
+
+        derivative_diff = np.linalg.norm(curr_derivative - derivatives, axis=1)
+
+        ratios = derivative_diff / eps
+        lipschitz_constant = np.max(ratios)
+
+        return max(lipschitz_constant, min_value)
+
     # Return the p-norm of a vector of an arbitrary p
-    def norm(vec, p):
+    def norm(vec, p, raise_to_power=True):
         if np.all(vec == 0):
             return 0
-        return (sum(np.abs(vec) ** p) ** (1. / p)).squeeze()
+        if raise_to_power:
+            return (sum(np.abs(vec) ** p) ** (1. / p)).squeeze()
+        else:
+            return sum(np.abs(vec) ** p).squeeze()

plotting.py
@@ -137,10 +137,17 @@ def generate_plots(params, values, plot_type="Loss", optimizer_types=None):
 
         plt.xlabel('Iterations')
         plt.ylabel(plot_type)
-
+        plt.ylim(top=average_values[0,0]*1.1)
         if plot_type == "Distance from Origin":
             plt.ylim(bottom=0)
-            plt.ylim(top=12)
+        else:
+            min_value = np.min(average_values)
+            if min_value < 0:
+                min_plot = min_value * 1.1
+            else:
+                min_plot = min_value * 0.9
+            plt.ylim(bottom=min_plot)
+
 
         plt.legend(optimizer_types, loc='upper right', prop={'size': 6})
 