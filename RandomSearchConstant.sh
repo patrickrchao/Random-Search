f2b986cd423a7153e25b1ebf09f3e7d9923956c1
Patrick Chao <patrickrchao@gmail.com>
2019-01-23 11:26:28 -0800

    Updated Images

--- RandomSearchConstant.sh
+++ RandomSearchConstant.sh
@@ -4,3 +4,9 @@ python3 main.py --function LOG --step_function CONSTANT --function_param 0.1 --c
 
 python3 main.py --function QUADRATIC  --step_function CONSTANT --function_param 0.2 --condition_num 5 --optimal --surface --contour
 python3 main.py --function LOG --step_function CONSTANT --function_param 0.0001 --condition_num 5 --optimal --surface --contour
+
+python3 main.py --function QUADRATIC  --step_function CONSTANT --function_param 2 --condition_num 1 --optimal --surface --contour --true_gradient
+python3 main.py --function LOG --step_function CONSTANT --function_param 0.1 --condition_num 1 --optimal --surface --contour --true_gradient
+
+python3 main.py --function QUADRATIC  --step_function CONSTANT --function_param 0.2 --condition_num 5 --optimal --surface --contour --true_gradient
+python3 main.py --function LOG --step_function CONSTANT --function_param 0.0001 --condition_num 5 --optimal --surface --contour --true_gradient
